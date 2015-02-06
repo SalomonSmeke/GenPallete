61598f6dbf9503cd78078ce492c428137a9caac7
SalomonSmeke <ssmeke@luc.edu>
2015-02-05 23:53:43 -0600

    Added tests in the main

--- src/GenPallete.java
+++ src/GenPallete.java
@@ -1,16 +1,33 @@
 public class GenPallete {
 
 	public static void main(String[] args) {
-		LWGenNeighbors A = new LWGenNeighbors();
 
-		//A.setEditable((byte)1);
-		String[] test = new String[2];
-		test[0] = "AA6c5F";
-		test[1] = "BB2034";
-		//A.setBases(test);
-		//A.setSteps((byte)3);
-		A.nextColors();
+		LWGenNeighbors N = new LWGenNeighbors();
+		String[] NB = new String[2];
+		NB[0] = "";
+		NB[1] = "";
+		N.setNeighbors(NB);
+		
+		N.nextColors();
+		
+		System.out.println(N.toString());
+		
+		LWGenPallete P = new LWGenPallete();
+		String PB = "";
+		P.setBase(PB);
+		
+		P.nextColors();
 
-		System.out.println(A.toString());
+		System.out.println(P.toString());
+		
+		LWGenBridge B = new LWGenBridge();
+		String[] BB = new String[2];
+		BB[0] = "";
+		BB[1] = "";
+		B.setBases(BB);
+		
+		B.nextColors();
+		
+		System.out.println(B.toString());
 	}
 }
